b9bee1e92d783f996be203139e8dcf5f3066cc8d
kanake10 <10kanake@gmail.com>
2022-09-02 17:52:56 +0300

    missing backslash

--- app/src/main/java/com/example/lovecalculator/common/Constants.kt
+++ app/src/main/java/com/example/lovecalculator/common/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.lovecalculator.common
 
 object Constants {
-    const val BASE_URL = "https://love-calculator.p.rapidapi.com"
+    const val BASE_URL = "https://love-calculator.p.rapidapi.com/"
     const val GET_PERCENTAGE = "getPercentage"
     const val FNAME_ID = "fname"
     const val SNAME_ID = "sname"
